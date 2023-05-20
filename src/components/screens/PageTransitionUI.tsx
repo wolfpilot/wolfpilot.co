cb0ed92982d36034acfe20e8123dfb452f25bdbe
Razvan Negrea <rznart@gmail.com>
2023-05-20 21:22:10 +0200

    Add a simple page transition animation

--- src/components/screens/PageTransitionUI.tsx
+++ src/components/screens/PageTransitionUI.tsx
@@ -1,9 +1,61 @@
+import { usePathname } from "next/navigation"
+import { AnimatePresence, motion } from "framer-motion"
+import styled from "styled-components"
+
+// Styles
+import { zIndexes } from "@/styles/zIndexes"
+
 export interface Props {
   children?: React.ReactNode
 }
 
-const PageTransitionUI: React.FC<Props> = ({ children }: Props) => (
-  <div>{children}</div>
-)
+// Constants
+const BASE_ANIM_DURATION = 1.5
+
+const PageTransitionUI: React.FC<Props> = ({ children }: Props) => {
+  const pathname = usePathname()
+
+  return (
+    <>
+      <AnimatePresence initial={false} mode="popLayout">
+        <AnimatedCover
+          key={`wipe-${pathname}`}
+          initial={false}
+          animate={{ scaleY: 0, translateY: 0 }}
+          exit={{ scaleY: [0, 1, 1], translateY: [0, 0, "-100%"] }}
+          transition={{
+            duration: BASE_ANIM_DURATION,
+            ease: [0.4, 0, 0.2, 1],
+          }}
+        />
+
+        <motion.div
+          key={pathname}
+          initial={{ opacity: 0, translateY: "20px" }}
+          animate={{ opacity: 1, translateY: "0px" }}
+          exit={{ opacity: 0, translateY: "0px" }}
+          transition={{
+            duration: BASE_ANIM_DURATION / 3,
+            delay: BASE_ANIM_DURATION / 3,
+            ease: [0.4, 0, 0.2, 1],
+          }}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </>
+  )
+}
+
+const AnimatedCover = styled(motion.div)`
+  position: fixed;
+  z-index: ${zIndexes.pageTransitionUI};
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: var(--c-accent1);
+  transform-origin: bottom center;
+`
 
 export default PageTransitionUI
