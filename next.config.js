133ed2a33f0d3ee021f3082aed7a45e0f4da9b7b
Razvan Negrea <rznart@gmail.com>
2023-06-16 14:10:44 +0200

    Fix slow on-demand image optimisation
    For more info, see:
    https://github.com/vercel/next.js/discussions/21294

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [320, 375, 425, 768, 1024, 1440, 1920, 2560, 3840],
+    minimumCacheTTL: 31536000,
   },
 }
 
