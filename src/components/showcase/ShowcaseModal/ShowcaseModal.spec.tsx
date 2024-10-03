dd77b1934324b428f37a81451526e7930b8a29b8
Razvan Negrea <rznart@gmail.com>
2024-10-03 12:09:51 +0200

    Update RTL, Jest and relevant tests

--- src/components/showcase/ShowcaseModal/ShowcaseModal.spec.tsx
+++ src/components/showcase/ShowcaseModal/ShowcaseModal.spec.tsx
@@ -16,7 +16,7 @@ import { ShowcaseProvider, State as ContextState } from "../Context"
 import ShowcaseModal from "./ShowcaseModal"
 
 describe("ShowcaseModal", () => {
-  it("renders correctly", () => {
+  it("renders correctly", async () => {
     const mockContextState: ContextState = {
       activeTag: "featured",
       activeItems: mockItems,
@@ -29,25 +29,15 @@ describe("ShowcaseModal", () => {
       </ShowcaseProvider>
     )
 
-    expect(
-      screen.getByRole("heading", { name: mockItems[0].name })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("img", { name: mockItems[0].image.alt })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("link", { name: "View on github.com" })
-    ).toBeInTheDocument()
-    expect(screen.getByRole("button", { name: /Close/i })).toBeInTheDocument()
-    expect(
-      screen.getByRole("button", { name: /View previous project/i })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("button", { name: /View next project/i })
-    ).toBeInTheDocument()
+    await screen.findByRole("heading", { name: mockItems[0].name })
+    await screen.findByRole("img", { name: mockItems[0].image.alt })
+    await screen.findByRole("link", { name: "View on github.com" })
+    await screen.findByRole("button", { name: /Close/i })
+    await screen.findByRole("button", { name: /View previous project/i })
+    await screen.findByRole("button", { name: /View next project/i })
   })
 
-  it("doesn't render if data is missing or null", () => {
+  it("doesn't render if data is missing or null", async () => {
     const mockContextState: ContextState = {
       activeTag: "featured",
       activeItems: null,
@@ -60,17 +50,17 @@ describe("ShowcaseModal", () => {
       </ShowcaseProvider>
     )
 
-    expect(container).toBeEmptyDOMElement()
+    await waitFor(() => expect(container).toBeEmptyDOMElement())
   })
 
-  it("doesn't render if no data is provided", () => {
+  it("doesn't render if no data is provided", async () => {
     const { container } = render(
       <ShowcaseProvider>
         <ShowcaseModal />
       </ShowcaseProvider>
     )
 
-    expect(container).toBeEmptyDOMElement()
+    await waitFor(() => expect(container).toBeEmptyDOMElement())
   })
 
   it("collapses the modal when clicking on the close button", async () => {
@@ -87,7 +77,7 @@ describe("ShowcaseModal", () => {
     )
 
     const closeBtn = screen.getByRole("button", { name: /Close/i })
-    userEvent.click(closeBtn)
+    await userEvent.click(closeBtn)
 
     await waitFor(() => expect(container).toBeEmptyDOMElement())
   })
@@ -105,7 +95,7 @@ describe("ShowcaseModal", () => {
       </ShowcaseProvider>
     )
 
-    userEvent.keyboard("{Escape}")
+    await userEvent.keyboard("{Escape}")
 
     await waitFor(() => expect(container).toBeEmptyDOMElement())
   })
@@ -124,24 +114,30 @@ describe("ShowcaseModal", () => {
     )
 
     // Check that there's no zoom out button initially
-    expect(
-      screen.queryByRole("button", { name: /Zoom out/i })
-    ).not.toBeInTheDocument()
+    await waitFor(() =>
+      expect(
+        screen.queryByRole("button", { name: /Zoom out/i })
+      ).not.toBeInTheDocument()
+    )
 
     // Click to zoom in
-    userEvent.click(screen.getByRole("button", { name: /Zoom in/i }))
+    await userEvent.click(screen.getByRole("button", { name: /Zoom in/i }))
 
     // Check that there's no zoom in button after zooming in
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole("button", { name: /Zoom in/i })
+    await waitFor(() =>
+      expect(
+        screen.queryByRole("button", { name: /Zoom in/i })
+      ).not.toBeInTheDocument()
     )
 
     // Click to zoom back out
-    userEvent.click(screen.getByRole("button", { name: /Zoom out/i }))
+    await userEvent.click(screen.getByRole("button", { name: /Zoom out/i }))
 
     // Check that there's no zoom out button after zooming out
-    await waitForElementToBeRemoved(() =>
-      screen.queryByRole("button", { name: /Zoom out/i })
+    await waitFor(() =>
+      expect(
+        screen.queryByRole("button", { name: /Zoom out/i })
+      ).not.toBeInTheDocument()
     )
   })
 
@@ -164,56 +160,41 @@ describe("ShowcaseModal", () => {
     })
 
     // Check that the initial contents are shown
-    expect(
-      screen.getByRole("heading", { name: mockItems[0].name })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("img", { name: mockItems[0].image.alt })
-    ).toBeInTheDocument()
-    expect(screen.getByText(mockItems[0].tagline)).toBeInTheDocument()
+    await screen.findByRole("heading", { name: mockItems[0].name })
+    await screen.findByRole("img", { name: mockItems[0].image.alt })
+    await screen.findByText(mockItems[0].tagline)
 
     // Click to see the next project
-    userEvent.click(nextBtn)
+    await userEvent.click(nextBtn)
 
     await waitForElementToBeRemoved(() =>
       screen.queryByRole("img", { name: mockItems[0].image.alt })
     )
 
     // Check that the next content is shown
-    expect(
-      screen.getByRole("heading", { name: mockItems[1].name })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("img", { name: mockItems[1].image.alt })
-    ).toBeInTheDocument()
-    expect(screen.getByText(mockItems[1].tagline)).toBeInTheDocument()
+    await screen.findByRole("heading", { name: mockItems[1].name })
+    await screen.findByRole("img", { name: mockItems[1].image.alt })
+    await screen.findByText(mockItems[1].tagline)
 
     // Click to see the previous project
-    userEvent.click(prevBtn)
+    await userEvent.click(prevBtn)
     await waitForElementToBeRemoved(() =>
       screen.queryByRole("img", { name: mockItems[1].image.alt })
     )
 
     // Click to see the previous project
-    userEvent.click(prevBtn)
-
+    await userEvent.click(prevBtn)
     await waitForElementToBeRemoved(() =>
       screen.queryByRole("img", { name: mockItems[0].image.alt })
     )
 
     // Check that the content has looped around to the last item
-    expect(
-      screen.getByRole("heading", {
-        name: mockItems[mockItems.length - 1].name,
-      })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByRole("img", {
-        name: mockItems[mockItems.length - 1].image.alt,
-      })
-    ).toBeInTheDocument()
-    expect(
-      screen.getByText(mockItems[mockItems.length - 1].tagline)
-    ).toBeInTheDocument()
+    await screen.findByRole("heading", {
+      name: mockItems[mockItems.length - 1].name,
+    })
+    await screen.findByRole("img", {
+      name: mockItems[mockItems.length - 1].image.alt,
+    })
+    await screen.findByText(mockItems[mockItems.length - 1].tagline)
   })
 })
